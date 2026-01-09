f0b3c69c565364ebdfffad8f46a0eb41c4adf8e3
Robert Antaluca <robert@antaluca.com>
2026-01-09 20:02:23 +0100

    added datasets

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from datasets import load_dataset 
 
 def main():
-
-    print("RobGPT - Main ")
+    print("="*50)
+    print("RobGPT - Chat Mode ")
     prompt = "What is your prompt?: \n"
     user_input = input(prompt)
+    print("="*50)
     print(f"Asking: {user_input} to RobGPT")
     
 
